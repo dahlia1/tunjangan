0b68843c79cb022f90fc4d04514c89ee54a051f0
dahlia1 <55682672+dahlia1@users.noreply.github.com>
2019-10-10 18:42:34 +0700

    Add files via upload

---
+++ PBO-10118004-Latihan17-Programtunjangan/JavaApplication52/src/javaapplication52/JavaApplication52.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication52;
+
+/**
+ *
+ * @author ASUS A411UF
+ */
+public class JavaApplication52 {
+
+    /**
+* @author
+ * Nama      : Dahlia
+ * Kelas     : PBO1
+ * NIM       : 10118004
+ * Deskripsi : Perulangan kelipatan
+ */
+
+    
+    public static void main(String[] args) {
+    double a = 3.5;
+    int i = 1;
+    
+    while(i<=10){
+        System.out.println(+a*i);
+        i++;
+            }
+    } 
+}
